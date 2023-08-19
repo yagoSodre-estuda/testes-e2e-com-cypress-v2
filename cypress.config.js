0601bb0a74dda16e3c161691d9e3f909bf54d420
yago.sodre <yago.sousa@estuda.com>
2023-08-19 17:25:50 -0300

    feat: logout test

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,10 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://notes-serverless-app.com',
+    env: {
+      viewportWidthBreakpoint: 768,
+    },
+    chromeWebSecurity: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
