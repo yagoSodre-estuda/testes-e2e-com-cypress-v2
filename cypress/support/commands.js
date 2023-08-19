d04cd3a68293a1e25ffbfcb39979e0780062c3e5
yago.sodre <yago.sousa@estuda.com>
2023-08-19 16:45:49 -0300

    feat: login test

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -14,3 +14,24 @@ Cypress.Commands.add('fillSignupFormAndSubmit', (email, password) => {
     cy.wait('@getNotes')
   })
 })
+
+Cypress.Commands.add('guiLogin', (
+  username = Cypress.env('USER_EMAIL'),
+  password = Cypress.env('USER_PASSWORD')
+) => {
+  cy.intercept('GET', '**/notes').as('getNotes')
+
+  cy.visit('/login')
+  cy.get('#email').type(username)
+  cy.get('#password').type(password, { log: false })
+  cy.contains('button', 'Login').click()
+  cy.wait('@getNotes')
+})
+
+Cypress.Commands.add('sessionLogin', (
+  username = Cypress.env('USER_EMAIL'),
+  password = Cypress.env('USER_PASSWORD')
+) => {
+  const login = () => cy.guiLogin(username, password)
+  cy.session(username, login)
+})
